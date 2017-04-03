5f979a3d3a41bd2f992875059f7451217fa0fbe1
Alexander Rutz <ar@animaux.de>
2017-04-03 09:32:10 +0200

    preg_replace /e + German
    - Replaced deprecated preg_replace /e in extension.driver.php
    - German translation

---
+++ lang/lang.de.php
@@ -0,0 +1,21 @@
+<?php
+
+	$about = array(
+		'name' => 'Deutsch',
+		'author' => array(
+			'name' => 'Alexander Rutz',
+			'email' => 'alex@animaux.de',
+			'website' => 'http://animaux.de'
+		),
+		'release-date' => '2017-04-03'
+	);
+
+	/**
+	 * Cross-Site Scripting (XSS) Filter
+	 */
+	$dictionary = array(
+
+		'Possible XSS attack detected in submitted data' => 
+		'In den gesendeten Daten wurde ein möglicher XSS-Angriff entdeckt',
+
+	);
